2482cea8a0aa311908df296156cbed4802fc6e85
Supriya030704 <mallepallysupriya394@.com>
2025-06-30 12:51:18 +0530

    first Project AI commit

---
+++ for.py
@@ -0,0 +1,7 @@
+data = [1,3,5,7,8,3,0,2]
+
+for i in data:
+    print("Data value is:",i)
+else:
+    print("Data value is completed")
+        
---
+++ while.py
@@ -0,0 +1,9 @@
+
+
+
+i=10
+
+while i>1:
+    print("I value is: ",i)
+    i=i+1  #i+=1
+    
